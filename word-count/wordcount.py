67129eb89c29a3708dfe57a2d2b8a79e3ece15b0
David Szlovak <szlovakdavid@gmail.com>
2016-02-14 07:16:53 +0000

    wordcount improvement

--- word-count/wordcount.py
+++ word-count/wordcount.py
@@ -1,26 +1,13 @@
-import unicodedata
+import re
 from collections import defaultdict
 
 def word_count(text):
     counter = defaultdict(lambda: 0)
-    for word in word_split(text):
-        counter[word]+=1
+    r = re.compile(r'[\W_]', re.UNICODE)
+    for word in r.split(decode_if_needed(text).lower()):
+        if word != '':
+            counter[word]+=1
     return dict(counter)
-
-def word_split(text):
-    words = []
-    word = ''
-    for char in decode_if_needed(text.lower()):
-        category = unicodedata.category(char)
-        if(category == 'Ll' or category == 'Nd'):
-            word += char
-        else:
-            if word != '':
-                words.append(word)
-            word = ''
-    if word != '':
-        words.append(word)
-    return words
 
 def decode_if_needed(string):
     try:
