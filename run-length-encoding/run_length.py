a10d1aa0826d12040bcc30ba0eb2bbbcef2f7cf9
David Szlovak <szlovakdavid@gmail.com>
2016-02-16 05:33:35 +0000

    Run length encoding finished

--- run-length-encoding/run_length.py
+++ run-length-encoding/run_length.py
@@ -1,18 +1,31 @@
 def encode(decoded):
     encoded = ''
-    last = ''
+    last = decoded[0]
     counter = 0
     for letter in decoded:
         if last == letter:
             counter += 1
         else:
-            if counter:
-                encoded+=counter+letter
-            else:
-                encoded+=letter
+            encoded += encode_letter(counter, last)
             last = letter
-            counter = 0
+            counter = 1
+    encoded += encode_letter(counter, last)
     return encoded
 
+def encode_letter(counter, last):
+    if counter > 1:
+        return unicode(counter)+last
+    else:
+        return last
+
 def decode(encoded):
+    decoded = ''
+    buffer = ''
+    for character in encoded:
+        if character.isdigit():
+            buffer += character
+        else:
+            buffer = buffer if buffer != '' else '1'
+            decoded += int(buffer)*character
+            buffer = ''
     return decoded
