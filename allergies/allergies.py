dfe098be0431cd38789bf9be0e2a54983f707d75
David Szlovak <szlovakdavid@gmail.com>
2016-02-18 22:09:36 +0000

    series unfinished

--- allergies/allergies.py
+++ allergies/allergies.py
@@ -1,5 +1,18 @@
 class Allergies:
-    def __init__(self, score):
-         self.score = score
+    allergies = [
+    'eggs',
+    'peanuts',
+    'shellfish',
+    'strawberries',
+    'tomatoes',
+    'chocolate',
+    'pollen',
+    'cats'
+    ]
+    lst = []
 
-    def is_allergic_to(what):
+    def __init__(self, score):
+        self.lst = list(allergy for allergy, number in zip(self.allergies, format(score, '08b')[::-1]) if number == '1')
+
+    def is_allergic_to(self, what):
+        return what in self.lst
