1111197530067bc1f89bbe0bf2c552ccb186c750
David Szlovak <szlovakdavid@gmail.com>
2016-03-25 09:39:01 +0000

    kindergarden unfinished

---
+++ kindergarten-garden/garden.py
@@ -0,0 +1,15 @@
+class Garden:
+    children = ['Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Fred', 'Ginny',
+    'Harriet', 'Ileana', 'Joseph', 'Kincaid', 'Larry']
+
+    plants = {'G': 'Grass', 'C': 'Clover', 'R': 'Radishes', 'V': 'Violets'}
+
+    def __init__(self, garden):
+        self.garden = []
+        for line in garden.split("\n"):
+
+
+
+    def plants(child):
+        index = children.index(child)
+        return self.garden[index]
