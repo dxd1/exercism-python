388907517a48064362210152de66627f602834c0
David Szlovak <szlovakdavid@gmail.com>
2016-03-25 13:01:04 +0000

    kindergarten

--- kindergarten-garden/garden.py
+++ kindergarten-garden/garden.py
@@ -1,15 +1,23 @@
 class Garden:
-    children = ['Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Fred', 'Ginny',
-    'Harriet', 'Ileana', 'Joseph', 'Kincaid', 'Larry']
+    plant_dictionary = {'G': 'Grass', 'C': 'Clover', 'R': 'Radishes', 'V': 'Violets'}
 
-    plants = {'G': 'Grass', 'C': 'Clover', 'R': 'Radishes', 'V': 'Violets'}
-
-    def __init__(self, garden):
+    def __init__(self, garden, students = []):
         self.garden = []
-        for line in garden.split("\n"):
+        if students == []:
+            self.children = ['Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Fred', 'Ginny',
+            'Harriet', 'Ileana', 'Joseph', 'Kincaid', 'Larry']
+        else:
+            students.sort()
+            self.children = students
+        lines = garden.split("\n")
+        for i in range(0, len(lines[0]), 2):
+            self.garden.insert(int(i/2), [])
+        for line in lines:
+            for i in range(0, len(line), 2):
+                self.garden[int(i/2)].append(self.plant_dictionary[line[i]])
+                self.garden[int(i/2)].append(self.plant_dictionary[line[i+1]])
 
 
 
-    def plants(child):
-        index = children.index(child)
-        return self.garden[index]
+    def plants(self, child):
+        return self.garden[self.children.index(child)]
