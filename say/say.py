82b9794a9e900f96c0a33e7bcd081011bc9fcbb8
David Szlovak <szlovakdavid@gmail.com>
2016-03-25 08:54:25 +0000

    say finished

--- say/say.py
+++ say/say.py
@@ -1,6 +1,7 @@
 import math
 
 number_dict = {
+    0: '',
     1: 'one',
     2: 'two',
     3: 'three',
@@ -29,40 +30,77 @@ number_dict = {
     80: 'eighty',
     90: 'ninety',
 }
-thousands = ['', 'thousand', 'million', 'billion'];
 def say(number):
     number = int(number)
     if number>999999999999 or number<0:
         raise AttributeError('Number is out of range')
     if number == 0:
         return 'zero'
-    result = []
     str_number = str(number)
+    thousands = slice_thousands(str_number)
+    text_thousands_list = text_thousands(thousands)
+    final = insert_thousands(text_thousands_list)
+    return ' '.join(final).replace('  ', ' ')
+
+def insert_thousands(text_thousands_list):
+    final = []
+    thousands = ['', 'thousand', 'million', 'billion']
+    for i in range(len(text_thousands_list)-1, -1, -1):
+        if text_thousands_list[i] != '':
+            final.insert(0, thousands[len(text_thousands_list)-1-i])
+            final.insert(0, text_thousands_list[i])
+    if final[-1] == '':
+        del final[-1]
+    return final
+
+def text_thousands(thousands):
+    text_thousands = []
+    if len(thousands) == 1:
+        text_thousands.append(digits_to_texts(thousands[0], False))
+    else:
+        for i in range(0, len(thousands)):
+            text_thousands.append(digits_to_texts(thousands[i], i==len(thousands)-1))
+    return text_thousands
+
+def slice_thousands(str_number):
+    thousand_list = []
+    thousand = ''
     for i in range(len(str_number)-1, -1, -1):
-        dec = len(str_number)-1-i
-        if dec % 3 == 2 and str_number[i] != '0':
-            result.insert(0, number_dict[int(str_number[i])] + ' hundred')
-            result.insert(1, 'and')
-        elif dec % 3 == 0:
-            if i==0:
-                result.insert(0, get_last_2(int(str_number[i])))
-                add_thousand = str_number[i] != '0'
-            else:
-                result.insert(0, get_last_2(int(str_number[i-1: i+1])))
-                add_thousand = str_number[i-1: i+1] != '00'
-                i-=1
-            if add_thousand:
-                result.insert(1,  thousands[dec/3])
+        thousand=str_number[i]+thousand
+        if len(thousand)==3:
+            thousand_list.insert(0, thousand)
+            thousand = ''
+        elif i==0:
+            thousand_list.insert(0, thousand)
+    return thousand_list
 
-    return ' '.join(result).strip(' ')
+def digits_to_texts(digits, add_and):
+    text = ''
+    if digits != '000':
+        if len(digits) == 3 and digits[0] != '0':
+            text += get_text(digits[0]) + ' hundred'
+            add_and = True
+        text += get_two_digits(digits[-2:], add_and)
 
-def get_last_2(number):
-    if number == 0:
-        return ''
+    return text
+
+def get_two_digits(str_number, add_and):
+    result = ''
+    number = int(str_number)
     if number<=20:
-        return number_dict[number]
+        result += get_text(number)
     else:
         ten = int(math.floor(number/10)*10)
-        return number_dict[ten] + '-' + number_dict[number-ten]
+        result += get_text(ten)
+        if ten != number:
+            result += '-' + get_text(number-ten)
+    if result != '' and add_and:
+        result = ' and ' + result
+    return result
 
-say(20)
+
+def get_text(number):
+    if number is int:
+        return number_dict[number]
+    else:
+        return number_dict[int(number)]
