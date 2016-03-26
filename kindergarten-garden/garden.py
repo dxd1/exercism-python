c2b672942ab3ad16f5e6fd960e10e8271e189cdd
David Szlovak <szlovakdavid@gmail.com>
2016-03-26 21:47:03 +0000

    kindergarden improvement

--- kindergarten-garden/garden.py
+++ kindergarten-garden/garden.py
@@ -1,23 +1,15 @@
 class Garden:
     plant_dictionary = {'G': 'Grass', 'C': 'Clover', 'R': 'Radishes', 'V': 'Violets'}
 
-    def __init__(self, garden, students = []):
+    def __init__(self, garden, students = None):
         self.garden = []
-        if students == []:
+        if students is None:
             self.children = ['Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Fred', 'Ginny',
             'Harriet', 'Ileana', 'Joseph', 'Kincaid', 'Larry']
         else:
-            students.sort()
-            self.children = students
-        lines = garden.split("\n")
-        for i in range(0, len(lines[0]), 2):
-            self.garden.insert(int(i/2), [])
-        for line in lines:
-            for i in range(0, len(line), 2):
-                self.garden[int(i/2)].append(self.plant_dictionary[line[i]])
-                self.garden[int(i/2)].append(self.plant_dictionary[line[i+1]])
-
-
+            self.children = sorted(students)
+        line1, line2 = garden.split("\n")
+        self.garden = list(zip(line1[::2], line1[1::2], line2[::2], line2[1::2]))
 
     def plants(self, child):
-        return self.garden[self.children.index(child)]
+        return [self.plant_dictionary[i] for i in self.garden[self.children.index(child)]]
