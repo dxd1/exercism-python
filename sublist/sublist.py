e664996a07cc0fe2034f3d3ab586b6b4aca0012d
David Szlovak <szlovakdavid@gmail.com>
2016-04-13 04:39:53 +0100

    sublist finished

--- sublist/sublist.py
+++ sublist/sublist.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 SUBLIST = 0
 SUPERLIST = 1
 EQUAL = 2
@@ -6,3 +8,19 @@ UNEQUAL = 3
 def check_lists(l1, l2):
     if l1 == l2:
         return EQUAL
+    elif is_sublist(l1, l2):
+        return SUBLIST
+    elif is_sublist(l2, l1):
+        return SUPERLIST
+    else:
+        return UNEQUAL
+
+def is_sublist(sublist, container):
+    if len(sublist) == 0:
+        return True
+    firsts = [i for i, j in enumerate(container) if j == sublist[0]]
+    if firsts:
+        for i in firsts:
+            if sublist == container[i:i+len(sublist)]:
+                return True
+    return False
